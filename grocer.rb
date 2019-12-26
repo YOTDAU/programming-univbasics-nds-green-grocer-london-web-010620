6f7e76311b12e79e370cc6cce8da5d4ed1d0aedb
Simon Tucker <sjtucker28@gmail.com>
2019-12-26 17:01:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -98,7 +98,8 @@ def checkout(cart, coupons)
     total += clearance_applied_cart[i][:price] * clearance_applied_cart[i][:count]
     i += 1 
   end
-  if total < 100
+  if total > 100
+    total -= total * 0.1 
   end
   total
     
