e8bc85e9927f028f994663af0ed018442e1b2a36
Simon Tucker <sjtucker28@gmail.com>
2019-12-26 16:32:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,8 @@ def apply_coupons(cart, coupons)
       else
         item_coupon = {
           :item => coupon_name, 
-          :price => coupons[i][:cost] / coupons[i][:num]
+          :price => coupons[i][:cost] / coupons[i][:num],
+          
         }
     
     
