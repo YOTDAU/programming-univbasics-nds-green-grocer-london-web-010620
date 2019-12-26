8c2b5dd66832bd850b33f772b9298bca2e16a446
Simon Tucker <sjtucker28@gmail.com>
2019-12-26 16:23:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,6 +47,12 @@ def apply_coupons(cart, coupons)
     coupon_name = "#{coupons[i][:item]} W/COUPON"
     item_coupon = find_item_by_name_in_collection(coupon_name, cart)
     
+     avocado_coupon = coupons.first
+        perfect_avocado_cart = [ avocado, avocado ]
+        consolidated_cart = consolidate_cart(perfect_avocado_cart)
+        coupon_applied_cart = apply_coupons(consolidated_cart, [avocado_coupon])
+        found_item = find_item_by_name_in_collection(item_with_coupon_applied_name, coupon_applied_cart)
+        expect(found_item).to_not be_nil, "After applying valid coupons make sure you add the applied coupon Hash"
     
     
   
