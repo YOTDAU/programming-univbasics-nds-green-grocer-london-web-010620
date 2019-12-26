af532031e2b3255408309c4ef43e7cd082a4469a
Simon Tucker <sjtucker28@gmail.com>
2019-12-26 16:48:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -75,7 +75,7 @@ def apply_clearance(cart)
   
   while i< cart.count do 
     if cart[i][:clearance] 
-      cart[i][:price] = ()
+      cart[i][:price] = (cart[i][:price] - cart[i])
   
   # Consult README for inputs and outputs
   #
